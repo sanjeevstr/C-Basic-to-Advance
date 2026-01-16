d61e8cd5fcc287bc3f2384679a5db51d9b5e3b2d
Sanjeev Sharma <sanjeevsharma10195@gmail.com>
2026-01-16 14:17:25 +0530

    Add files via upload

---
+++ Variable, Operator and Input, output/Variable.c
@@ -0,0 +1,15 @@
+// Variable Declaration and Initialization
+
+#include <stdio.h>
+int main()
+{
+    int age = 20;   
+    char s[10] = {"Hi"} ; 
+    float marks = 85.5; 
+    double pi = 3.14159;
+
+    printf("%d\n",age);
+    printf("%s\n",s);
+    printf("%f\n",marks);
+    printf("%lf\n",pi);
+}
