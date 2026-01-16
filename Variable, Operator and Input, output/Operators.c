9b47efebd1f4a897f0bbeb8a6eaa9e2a5efe1bf3
Sanjeev Sharma <sanjeevsharma10195@gmail.com>
2026-01-16 14:17:48 +0530

    Add files via upload

---
+++ Variable, Operator and Input, output/Operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 3;
+
+    printf("%d\n", a + b);  // 13
+
+    printf("%d\n", a - b);  // 7
+
+    printf("%d\n", a * b);  // 30
+
+    printf("%d\n", a / b);  // 3
+
+    printf("%d\n", a % b);  // 1
+
+
+}
